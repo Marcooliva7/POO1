6798c7fa6a1377df941c96a087c5823c9dabc6fa
Marcooliva7” <oliva312003@gmail.com>
2024-11-14 22:37:55 -0600

    Actividad 1

---
+++ p139_ArchivoEstudiante/App.java
@@ -0,0 +1,11 @@
+package p139_ArchivoEstudiante;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[2J");
+            Proceso procesa = new Proceso();
+            procesa.mostrarMenu();
+    }
+    
+    
+}
