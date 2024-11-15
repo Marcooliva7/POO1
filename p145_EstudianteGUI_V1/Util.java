ebbb8af4d6b403789de236a53cb4c87e680dbe34
Marcooliva7” <oliva312003@gmail.com>
2024-11-14 22:53:37 -0600

    Actividad 1

---
+++ p145_EstudianteGUI_V1/Util.java
@@ -0,0 +1,14 @@
+package p145_EstudianteGUI_V1;
+
+import java.util.ArrayList;
+
+public class Util {
+    public static ArrayList<Estudiante> inicializarDatos() {
+        ArrayList<Estudiante> datos = new ArrayList<>();
+        datos.add(new Estudiante("Juan perez", 22,34.5,"jperez@gmail.com"));
+        datos.add(new Estudiante("Maria Jimenez", 18,22.5,"mjimenez@gmail.com"));
+        datos.add(new Estudiante("Carlos Fuentes", 30,45,"cfuentes@gmail.com"));
+        return datos;
+
+    }
+}
